4387ca3f06afcc15963520892d5aef4574b1dca1
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2017-12-20 13:14:01 -0500

    stuff

--- Lab 4.2/src/CSVUtilities.java
+++ Lab 4.2/src/CSVUtilities.java
@@ -15,37 +15,47 @@ public class CSVUtilities {
 	{
 		FileReader reader = null;
 		BufferedReader filein = null;
-		try {
+		try 
+		{
 			reader = new FileReader(csv);
 
-		filein = new BufferedReader(reader);
+			filein = new BufferedReader(reader);
 		}
 
-		catch (FileNotFoundException fnfe) {
-		System.out.println("ERROR: File not found!");
-
-
+		catch (FileNotFoundException fnfe) 
+		{
+			System.out.println("ERROR: File not found!");
+		
 		}
 
 	}
 
 	public List<String> getColumnHeaders()
 	{
+		ArrayList<String> headers = new ArrayList<String>();
 		
+		headers.add(e);
+		return headers;
 	}
 	
 	public List<String> getDataString(int column)
 	{
+		ArrayList<String> data = new ArrayList<String>();
 		
+		return data;
 	}
 	
 	public List<Integer> getDataInt(int column)
 	{
+		ArrayList<Integer> dataInt = new ArrayList<Integer>();
 		
+		return dataInt;
 	}
 	
 	public List<Double> getDataDouble(int column)
 	{
+		ArrayList<Double> dataDouble = new ArrayList<Double>();
 		
+		return dataDouble;
 	}
 }
--- Lab 4.2/src/Runner.java
+++ Lab 4.2/src/Runner.java
@@ -1,8 +1,9 @@
 
 public class Runner {
 
-	public Runner() {
-		// TODO Auto-generated constructor stub
+	public static void main (String[] args)
+	{
+		
 	}
 
 }
